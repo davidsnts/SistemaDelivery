f7134c81128f8faa1735e2cdc42ab627808bcf29
davidsnts <david.santos@delage.com.br>
2024-09-23 21:58:47 -0300

    Criação do projeto Web

---
+++ Sistema Delivery/SistemaDeliveryWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaDeliveryWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
